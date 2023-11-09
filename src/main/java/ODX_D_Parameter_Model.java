ce3844bfb632943fc97c23ed46d4c2b6da02c857
Cory Marleau <marleau24@up.edu>
2023-11-08 16:26:56 -0800

    Adding Jackson JSON formatter

--- src/main/java/ODX_D_Parameter_Model.java
+++ src/main/java/ODX_D_Parameter_Model.java
@@ -1,11 +1,7 @@
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data               // Generates getters, setters, toString, equals, and hashCode methods
-@NoArgsConstructor  // Generates a no-argument constructor
-@AllArgsConstructor // Generates a constructor with all fields as arguments
+@Data
 @Builder
 public class ODX_D_Parameter_Model {
     private String semantic;
@@ -13,17 +9,6 @@ public class ODX_D_Parameter_Model {
     private String longName;
     private String bytePosition;
     private String codedValue;
-
-    @Override
-    public String toString() {
-        return "ODX_D_Parameter_Model {" + "\n" +
-                "  Semantic: " + semantic + "\n" +
-                "  Short Name: " + shortName + "\n" +
-                "  Long Name: " + longName + "\n" +
-                "  Byte Position: " + bytePosition + "\n" +
-                "  Coded Value: " + codedValue + "\n" +
-                "}";
-    }
 }
 
 
