b218660cc943334c7f0a4c72fe9afefe6288c730
Cory Marleau <marleau24@up.edu>
2023-11-10 15:07:32 -0800

    Creates output Json file

--- src/main/java/Parser/ODX_D_Deserializer.java
+++ src/main/java/Parser/ODX_D_Deserializer.java
@@ -1,9 +1,13 @@
 package Parser;
 
 import Models.ODX_Model;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,10 +16,12 @@ public class ODX_D_Deserializer {
     private static final String FILE_PATH = "src/main/resources/RDF03T_04.02.60.odx-d";
     private final XmlMapper xmlMapper;
     private final ObjectMapper mapper;
+    private final ObjectWriter writer;
 
     public ODX_D_Deserializer() {
         mapper = new ObjectMapper();
         xmlMapper = new XmlMapper();
+        writer = mapper.writer(new DefaultPrettyPrinter());
         xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
@@ -23,7 +29,8 @@ public class ODX_D_Deserializer {
         try {
             ODX_D_Deserializer deserializer = new ODX_D_Deserializer();
             ODX_Model odxModel = deserializer.deserializeODX(FILE_PATH);
-            //TODO: Any Transformations to the JSON will be performed here
+            //TODO: Any transformations to the JSON will be performed here
+            deserializer.writeToFile(odxModel);
             String jsonString = deserializer.serializeToJson(odxModel);
             System.out.println(jsonString);
         } catch (IOException e) {
@@ -38,6 +45,10 @@ public class ODX_D_Deserializer {
 
     public String serializeToJson(ODX_Model model) throws IOException {
         return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+    }
+
+    public void writeToFile(ODX_Model model) throws IOException {
+        writer.writeValue(new File("src/main/java/Output/output.json"), model);
     }
 }
 
