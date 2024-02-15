28cb5a5277e0f6970404c28765eedc9211406c2a
jkatayama24 <katayama24@up.edu>
2024-02-14 17:29:02 -0800

    Adding Postman/Docker classes to call, starting compare class too

--- src/main/java/Parser/ODX_D_Deserializer.java
+++ src/main/java/Parser/ODX_D_Deserializer.java
@@ -9,6 +9,12 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -64,8 +70,92 @@ public class ODX_D_Deserializer {
     public void writeToFile(ODX_D_Model model, String filePath) throws IOException {
         writer.writeValue(new File(filePath), model);
     }
+
     public void writeToFile(ODX_E_Model model, String filePath) throws IOException {
         writer.writeValue(new File(filePath), model);
     }
 }
+@RestController
+class ODX_D_Parser {
+    private final XmlMapper xmlMapper;
+    private final ObjectMapper mapper;
 
+    public ODX_D_Parser() {
+        mapper = new ObjectMapper();
+        xmlMapper = new XmlMapper();
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+    @PostMapping("/ODX-D")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            String content = new String(file.getBytes());
+            ODX_D_Parser deserializer = new ODX_D_Parser();
+            ODX_D_Model odxModel = deserializer.deserializeODX(content);
+
+            String jsonString = deserializer.serializeToJson(odxModel);
+            return new ResponseEntity<>(jsonString, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace(); // Print the exception details for debugging
+            return new ResponseEntity<>("Error uploading or processing the file", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ODX_D_Model deserializeODX(String content) throws IOException {
+        return xmlMapper.readValue(content, ODX_D_Model.class);
+    }
+
+    public String serializeToJson(ODX_D_Model model) throws IOException {
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+    }
+}
+
+@RestController
+class ODX_E_Parser {
+    private final XmlMapper xmlMapper;
+    private final ObjectMapper mapper;
+
+    public ODX_E_Parser() {
+        mapper = new ObjectMapper();
+        xmlMapper = new XmlMapper();
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @PostMapping("/ODX-E")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            String content = new String(file.getBytes());
+            ODX_E_Parser deserializer = new ODX_E_Parser();
+            ODX_E_Model odxModel = deserializer.deserializeODX(content);
+
+            String jsonString = deserializer.serializeToJson(odxModel);
+            return new ResponseEntity<>(jsonString, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace(); // Print the exception details for debugging
+            return new ResponseEntity<>("Error uploading or processing the file", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ODX_E_Model deserializeODX(String content) throws IOException {
+        return xmlMapper.readValue(content, ODX_E_Model.class);
+    }
+
+    public String serializeToJson(ODX_E_Model model) throws IOException {
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+    }
+}
+
+
+@RestController
+class compare{
+    @PostMapping("/compare")
+    public ResponseEntity<String> compareUpload(@RequestParam("file") MultipartFile file,@RequestParam("file") MultipartFile file2) {
+        try {
+            String content = new String(file.getBytes());
+            String content2 = new String(file2.getBytes());
+            return new ResponseEntity<>("Upload Successful", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace(); // Print the exception details for debugging
+            return new ResponseEntity<>("Error uploading or processing the file", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
